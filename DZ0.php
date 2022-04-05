e75e01bcd5b089f8791a0976cef51ebb8e9cb96d
Marat Zhanpeisov <themjdex@gmail.com>
2022-04-05 15:22:49 +0500

    homework 0

--- DZ0.php
+++ DZ0.php
@@ -1,2 +1,18 @@
 <?php
-$q = 1;
+// CONST testConstanta = 'asd';
+const TEST_CONSTANTA = 'asd';
+
+// $user_name = "Igor";
+$userName = "Igor";
+
+// function show_something(){ RETURN 'SOMETHING';}
+function showSomething()
+{
+    return 'SOMETHING';
+}
+
+// if (1==1) {}echo 'hi';
+
+if (1 == 2) {
+    echo 'hi';
+}
